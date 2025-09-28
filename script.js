0bf6fdd6766c13da7288d131b9d80b39becbee9c
rsingh174 <rajputsinghraghavendra@gmail.com>
2025-09-29 01:32:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar color change on scroll
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
